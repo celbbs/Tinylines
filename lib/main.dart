6ceefd2b0cded9ff308c8483206270d373629962
ajoffr <annajewel2018@yahoo.com>
2025-11-03 02:06:52 -0600

    Changed action button color to pink

--- lib/main.dart
+++ lib/main.dart
@@ -115,7 +115,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.pinkAccent.shade100,
+        child: const Icon(Icons.add, color: Colors.white),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
