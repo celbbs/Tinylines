69a48744e17f184b591905e7a58e98e5bedb0b4d
celbbs <cbabbs@my.chemeketa.edu>
2026-02-18 13:16:57 -0800

    Merge kuena-auth-prep into celia-dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'providers/journal_provider.dart';
 import 'screens/auth_screen.dart';
 import 'utils/app_theme.dart';
-
 import 'services/auth_service.dart';
 import 'services/fake_auth_service.dart';
 
@@ -27,32 +25,6 @@ class TinyLinesApp extends StatelessWidget {
         theme: AppTheme.lightTheme,
         debugShowCheckedModeBanner: false,
         home: const AuthScreen(),
-      ),
-    );
-  }
-}
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/journal_provider.dart';
-import 'screens/home_screen.dart';
-import 'utils/app_theme.dart';
-
-void main() {
-  runApp(const TinyLinesApp());
-}
-
-class TinyLinesApp extends StatelessWidget {
-  const TinyLinesApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => JournalProvider(),
-      child: MaterialApp(
-        title: 'TinyLines',
-        theme: AppTheme.lightTheme,
-        debugShowCheckedModeBanner: false,
-        home: const HomeScreen(),
       ),
     );
   }
